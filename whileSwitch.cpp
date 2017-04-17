94628e280044afd5aaec9d5a24ff26faafebcf30
Daniel Elliott <danielelliott3d@gmail.com>
2017-04-17 12:39:28 +1200

    Create whileSwitch.cpp

---
+++ whileSwitch.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+//http://en.cppreference.com/w/cpp/language/switch
+
+
+int main()
+{
+    bool isPlaying = true;
+    char command;
+    
+    while (isPlaying){
+        
+        cout << "What would you like to do? Type 'p' to play the game or 'e' to exit" << endl;
+        cin >> command;
+        
+        switch(command) {
+            case 'P' :
+            case 'p' :  cout << "playing game" << endl;
+                        break;
+                        
+            case 'E' :
+            case 'e' :  cout << "exiting game" << endl;
+                        isPlaying = false;
+                        break;
+                        
+            default :   cout << "you typed the wrong thing. Please try again" << endl;
+        }
+        
+    }
+    
+    cout << "exiting" << endl;
+
+    return 0;
+}
+
+
+    
