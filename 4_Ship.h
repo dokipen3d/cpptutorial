b40b043fa65ff354cba4a5e9f97b2d7a1b29fa07
dokipen <dokipen@dokipen.fritz.box>
2017-05-02 21:35:55 +1200

    fix separation of files and duplicate symbols by making globals static

---
+++ 4_Ship.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "4_SeaItem.h"
+#include "4_BattleShipUtility.hpp"
+#include <iostream>
+
+//forward declaration
+class Player;
+
+namespace BattleShip{
+
+    class Ship : public SeaItem {
+        public:
+            Ship(eShipStatus shipType, Player* playerPtrIn);
+            
+            virtual eShipStatus Hit() override;
+                //could hoist into SeaItem but not ideal as it's specific to ships but makes polymorphism easier
+            //later will get rid of these but for now ship keeps track of where it is.
+            //this provides a good way to show dynamic cast? good way to intriduce pointers?
+            //or test if shit and make a local reference! yay references.
+            eOrientation orientation;
+            //might be col or row depending on orientation 
+            int index;
+            //so we can search and count damage
+            int fromPos;
+            int toPos;
+
+        
+            
+        protected:
+
+            Player* player;
+            
+            
+    };
+}
